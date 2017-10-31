529eed994a1a0206df780bfcfa21cdf8ccaf5ef7
Carrie Ravotta <carrie.peary@gmail.com>
2017-10-30 22:07:14 -0400

    Working on CSV

--- blizzcon17/blizzcon.py
+++ blizzcon17/blizzcon.py
@@ -1,31 +1,12 @@
 #!/bin/python
-import os
-import sys
+import csv
 from bs4 import BeautifulSoup
 
 with open('data.txt') as file:
     sdata = file.readlines()
 
-# first attempt
-soup = BeautifulSoup(str(sdata), "html.parser")
-mydivs = soup.find_all("article")
-
-final = []
-for div in mydivs:
-        title = div.findChild('h1')
-        times = div.findChildren('time')
-        location = div.findChild('div', class_='event-stage')
-        if title != None:
-            final.append(title)
-            print title
-            print '----'
-            print str(location)
-            print '----'
-            print str(times)
-            print '====='
-
-f = open('output.txt', 'w')
-f.writelines([str(line) + "\n" for line in final])
+csvfile = open('output.csv', 'wb')
+spamwriter = csv.writer(csvfile, delimiter='\t')
 
 # second attempt
 soup = BeautifulSoup(str(sdata), "html.parser")
@@ -37,16 +18,34 @@ for div in mydivs:
         times = div.findChildren('time')
         location = div.findChild('div', class_='event-stage')
         description = div.findChild('div', class_='event-description')
-        if title != None:
-            final.append(title)
-            print title
-            print '----'
-            print str(location)
-            print '----'
-            print str(times)
-            print '----'
-            print str(description)
-            print '====='
 
-f = open('output.txt', 'w')
-f.writelines([str(line) + "\n" for line in final])
+        event = []
+
+        if title != None:
+            #print title.get_text()
+            event.append(title.get_text())
+
+            #print str(location.get_text())
+            event.append(location.get_text())
+
+            #print str(times)
+            if len(times) == 4:
+                stime = div.find_all('time', itemprop='startTime')
+                etime = div.find_all('time', itemprop='endTime')
+
+                event.append(stime[0].attrs.get('datetime', ''))
+                event.append(etime[0].attrs.get('datetime', ''))
+                event.append(stime[1].attrs.get('datetime', ''))
+                event.append(etime[1].attrs.get('datetime', ''))
+
+            if description != None:
+                #print str(description.get_text())
+                event.append(description.get_text())
+
+        final.append([str(line) for line in event])
+        csvfile.write('\t'.join(event))
+#f = open('output.csv', 'w')
+#f.writelines([str(line) + '\n' for line in final])
+# with open('output.csv', 'wb') as csvfile:
+#     spamwriter = csv.writer(csvfile, delimiter='\t')
+#     spamwriter.writerows([str(line) for line in final])
